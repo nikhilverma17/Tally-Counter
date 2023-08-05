fc57f48eab2148c88fe429f55d6d7b2d9c15f080
Nikhil Verma <63592210+nikhilverma17@users.noreply.github.com>
2023-08-05 12:15:12 +0530

    uplod

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count,setCount]=useState(0)
+  function increase(){
+    setCount(count+1);
+  }
+  function decrease(){
+    setCount(count-1);
+  }
+  function reset(){
+    setCount(0);
+  }
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <button onClick={reset}>↻</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import "./App.css"
+function Footer(){
+    return(
+        <footer>Copyright Nikhil Verma</footer>
+    )
+}
+export default Footer;
